4e4786e04a174ecb61b0949483c79b52eb2d0817
Peter <peterohwofasa@yahoo.com>
2024-04-09 15:50:34 +0200

    First commit - chat server

---
+++ src/main/java/com/chatserver/Entities/Message.java
@@ -0,0 +1,25 @@
+package com.chatserver.Entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String sender;
+    private String content;
+    private long timestamp;
+
+}
